80780c28142e89aea8cea1438edd8f88e2fec51f
Zackarias00 <zacharywarfelresume@gmail.com>
2021-08-16 20:12:47 -0500

    Let's try this again.

---
+++ OkayReally/TheRecipeProject/src/main/java/org/LiftOff/TheRecipeProject/TheRecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package org.LiftOff.TheRecipeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRecipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRecipeProjectApplication.class, args);
+	}
+
+}
