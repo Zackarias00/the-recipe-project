5a9283d9a05ed0e832f3a95324c2d3038ba0ab42
Zackarias00 <zacharywarfelresume@gmail.com>
2021-08-17 18:22:59 -0500

    Attempting to resolve gradle issues.

---
+++ TheRecipeProject/src/test/java/org/LiftOff/TheRecipeProject/TheRecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.LiftOff.TheRecipeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRecipeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
